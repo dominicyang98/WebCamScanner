be2c055e1dab0edc6e140329f2e9b5d25a01caae
Man YANG <60531440+dominicyang98@users.noreply.github.com>
2023-04-14 10:25:02 +0200

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="header" style="background-color:lightblue; text-align:center ;">QR CODE SCANNER</div>
+  <div id="reader" width="600px"></div>
+  
+</template>
+
+<script>
+  // To use Html5QrcodeScanner (more info below)
+  import {Html5QrcodeScanner} from "html5-qrcode";
+
+  // To use Html5Qrcode (more info below)
+  import {Html5Qrcode} from "html5-qrcode";
+
+  function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+  }
+
+  function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+    console.warn(`Code scan error = ${error}`);
+  }
+
+  if (process.client){
+    //test
+
+    let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    { fps: 10, qrbox: {width: 250, height: 250} },
+    /* verbose= */ false);
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
+  }
+
+</script>
