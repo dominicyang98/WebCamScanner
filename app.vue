e6e1f0f3ba704c3a066bf7651eb0d5ccd1ae8476
dominicyang98 <236195869@qq.com>
2023-04-21 10:41:36 +0200

    Details added

--- app.vue
+++ app.vue
@@ -1,69 +1,84 @@
 <template>
-  <div id="header" style="background-color:lightblue; text-align:center ;">QR CODE SCANNER TEST</div>
-  <div id="reader" width="600px"></div>
+    <div id="header" style="background-color:lightblue; text-align:center ;">QR CODE SCANNER TEST</div>
+    <div id="reader" width="600px"></div>
 
 </template>
 
 <script>
 
-  const consoleOutput = window.consoleOutput || {};
+const consoleOutput = window.consoleOutput || {};
 
-  var message = 'This is a unique message.';
-
-
-
-  export default {
-      mounted(){
-          const script = document.createElement('script')
-          script.src = './clientConsole.js'
-          document.head.appendChild(script)
-      }
-  }
-  // To use Html5QrcodeScanner (more info below)
-  import {Html5QrcodeScanner} from "html5-qrcode";
- // var resultContainer = document.getElementById('reader')
-
-  // To use Html5Qrcode (more info below)
-  // import {Html5Qrcode} from "html5-qrcode";
+var message = 'This is a unique message.';
 
 
-  async function onScanSuccess(decodedText, decodedResult) {
-      // handle the scanned code as you like, for example:
-      if (!consoleOutput[message]) {
-          message = decodedResult;
-          consoleOutput[message] = true;
-          console.log(`Code matched = ${decodedText}`, message);
-          this.targetObject = await findInfoDetails(message);
-          console.log(this.targetObject);
-      }
-  }
 
-  function onScanFailure(error) {
+export default {
+    mounted(){
+        const script = document.createElement('script')
+        script.src = './clientConsole.js'
+        document.head.appendChild(script)
+    }
+}
+// To use Html5QrcodeScanner (more info below)
+import {Html5QrcodeScanner} from "html5-qrcode";
+import jsonData from "./inventory-details.json";
+
+export default {
+    data() {
+        return {
+            foundItem: null
+        };
+    },
+    created() {
+        const searchId = this.getSearchId();
+        this.foundItem = jsonData.data.find(item => item.id === searchId);
+    },
+    methods: {
+        getSearchId() {
+            // 假设这里是某个函数返回的searchId值
+            return "UNG6300";
+        }
+    }
+};
+</script>
+
+async function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    if (!consoleOutput[message]) {
+        message = decodedResult;
+        consoleOutput[message] = true;
+        console.log(`Code matched = ${decodedText}`, message);
+        this.targetObject = await findInfoDetails(message);
+        console.log(this.targetObject);
+    }
+}
+
+function onScanFailure(error) {
     // handle scan failure, usually better to ignore and keep scanning.
     // for example:
     console.warn(`Code scan error = ${error}`);
-  }
+}
 
-  async function findInfoDetails(id) {
-      try {
-          const response = await this.$axios.$get('/inventory-details.json')
-          const targetObject = response.find(obj => obj.id === id);
-          return targetObject;
-      } catch (error) {
-          console.error(error)
-          return null;
-      }
-  }
+async function findInfoDetails(id) {
+    try {
+        const response = await this.$axios.$get('/inventory-details.json')
+        const targetObject = response.find(obj => obj.id === id);
+        return targetObject;
+    } catch (error) {
+        console.error(error)
+        return null;
+    }
+}
 
 
-  if (process.client){
+if (process.client){
 
     let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: {width: 250, height: 250} },
-    /* verbose= */ false);
+        "reader",
+        { fps: 10, qrbox: {width: 250, height: 250} },
+        /* verbose= */ false);
     html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 
-  }
+}
 
 </script>
